bebf3703f48108d98868ad361e86077f789a617f
Triste <1182511338@qq.com>
2017-01-22 17:48:19 +0800

    add DataHandler.h

---
+++ Server/CloudServer/DataHandler.h
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <time.h>
+#include <sys/time.h>
+using namespace std;
+
+class DataHandler{
+public:
+	DataHandler(){
+
+	}
+
+	void setRecvData(int d, int i){
+		recvData[i] = d;
+	}
+
+	void printRecvData(){
+		gettimeofday(&tv,0);  
+		unsigned long timenow = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+		cout<<endl<<"timenow:-----------------"<<timenow<<endl;
+		cout<<"10 R:";
+		for(int i=0;i<10;++i)
+			cout<<recvData[i]<<" ";
+
+		cout<<endl<<"time:"<<recvData[46]<<" "<<"1st jy901:";
+		for(int i=10;i<19;++i)
+			cout<<recvData[i]<<" ";
+
+		cout<<endl<<"time:"<<(recvData[46]+recvData[47])<<" "<<"2nd jy901:";
+		for(int i=19;i<28;++i)
+			cout<<recvData[i]<<" ";
+
+		cout<<endl<<"time:"<<(recvData[46]+recvData[48])<<" "<<"3rd jy901:";
+		for(int i=28;i<37;++i)
+			cout<<recvData[i]<<" ";
+
+		cout<<endl<<"time:"<<(recvData[46]+recvData[49])<<" "<<"4th jy901:";
+		for(int i=37;i<46;++i)
+			cout<<recvData[i]<<" ";
+
+		cout<<endl;
+
+		// data[i/3] = ((float)d)*16/32768;
+		// data[i/3] = (float)d*2000/32768;
+		// data[i/3] = (float)d*180/32768;
+	}
+
+	~DataHandler(){
+
+	}
+
+private:
+	int recvData[50];
+	struct timeval tv;
+	unsigned long lasttime;
+	int lastZAngle;
+};

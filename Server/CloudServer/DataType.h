bf92f8cbf0d23f273e572e5d6502caadf05fb51d
Triste <1182511338@qq.com>
2017-01-23 22:30:10 +0800

    add angle's quaternion function

--- Server/CloudServer/DataType.h
+++ Server/CloudServer/DataType.h
@@ -8,9 +8,9 @@ public:
 	Acceleration(){}
 
 	Acceleration(int _x,int _y,int _z){
-		x = (float)_x*16/32768;
-		y = (float)_y*16/32768;
-		z = (float)_z*16/32768;
+		x = (float)_x/32768*16;
+		y = (float)_y/32768*16;
+		z = (float)_z/32768*16;
 	}
 
 //private:
@@ -46,8 +46,15 @@ public:
 		rad_x = (float)_x*PI/32768;
 		rad_y = (float)_y*PI/32768;
 		rad_z = (float)_z*PI/32768;
-		cout<<"origin:"<<x<<" "<<y<<" "<<z<<", "<<"cal:"<<rad_x<<" "<<rad_y<<" "<<rad_z<<endl;
+		cout<<"origin:"<<x<<" "<<y<<" "<<z<<", "<<"cal:"<<rad_x<<" "<<rad_y<<" "<<rad_z<<endl;	
+		q[0] = cos(rad_x/2)*cos(rad_y/2)*cos(rad_z/2) + sin(rad_x/2)*sin(rad_y/2)*sin(rad_z/2);
+		q[1] = sin(rad_x/2)*cos(rad_y/2)*cos(rad_z/2) - cos(rad_x/2)*sin(rad_y/2)*sin(rad_z/2);
+		q[2] = cos(rad_x/2)*sin(rad_y/2)*cos(rad_z/2) + sin(rad_x/2)*cos(rad_y/2)*sin(rad_z/2);
+		q[3] = cos(rad_x/2)*cos(rad_y/2)*sin(rad_z/2) - sin(rad_x/2)*sin(rad_y/2)*cos(rad_z/2);
+		
 	}
+
+
 
 //private:
 	float x;
@@ -56,4 +63,5 @@ public:
 	float rad_x;
 	float rad_y;
 	float rad_z;
+	float q[4];
 };

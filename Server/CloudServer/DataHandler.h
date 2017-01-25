decc9c68f1ecee5b2dc7b8b5f76f7e38f9c4b2e4
Triste <1182511338@qq.com>
2017-01-25 23:39:26 +0800

    change datahandler.h by datalist.h

--- Server/CloudServer/DataHandler.h
+++ Server/CloudServer/DataHandler.h
@@ -19,6 +19,35 @@ public:
 		recvData[i] = d;
 	}
 
+	void handleRecvData(){
+		int time[4];
+		time[0] = recvData[46];
+		time[1] = recvData[46]+recvData[47];
+		time[2] = recvData[46]+recvData[48];
+		time[3] = recvData[46]+recvData[49];
+		for(int i=0;i<4;++i){
+			cout<<"construct dataunit-"<<i<<":"<<endl;
+			dataUnit[i] = DataUnit(recvData+(i*9+10),time[i]);
+			cout<<"datahandler time:"<<dataUnit[i].getTime()<<endl;
+			dataList->addDataUnit(recvData+(i*9+10),time[i]);
+		}
+
+		float a = (lastDataUnit.getAcc().getX()+dataUnit[0].getAcc().getX())/2*;
+		float t = dataUnit[0].getTime() - lastDataUnit.getTime();
+		v += (a*9.8*t/1000);
+		for(int i=1;i<4;++i){
+			a = (dataUnit[i-1].getAcc().getX()+dataUnit[i].getAcc().getX())/2*;
+			t = dataUnit[i].getTime() - dataUnit[i-1].getTime();
+			v += (a*9.8*t/1000);
+		}
+
+		cout<<"-----------------v:"<<v<<endl;
+
+		// data[i/3] = ((float)d)*16/32768;
+		// data[i/3] = (float)d*2000/32768;
+		// data[i/3] = (float)d*180/32768;
+	}
+
 	void printRecvData(){
 		gettimeofday(&tv,0);  
 		unsigned long timenow = tv.tv_sec * 1000 + tv.tv_usec / 1000;
@@ -27,6 +56,7 @@ public:
 		for(int i=0;i<10;++i)
 			cout<<recvData[i]<<" ";
 
+		
 		cout<<endl<<"time:"<<recvData[46]<<" "<<"1st jy901:";
 		for(int i=10;i<19;++i)
 			cout<<recvData[i]<<" ";
@@ -45,18 +75,6 @@ public:
 
 		cout<<endl;
 
-
-		for(int i=0;i<4;++i){
-			cout<<"construct dataunit-"<<i<<":"<<endl;
-			dataUnit[i] = DataUnit(recvData+(i*9+10));
-			cout<<"datahandler time:"<<dataUnit[i].time<<endl;
-			dataList->addDataUnit(dataUnit[i]);
-
-		}
-
-		// data[i/3] = ((float)d)*16/32768;
-		// data[i/3] = (float)d*2000/32768;
-		// data[i/3] = (float)d*180/32768;
 	}
 
 	~DataHandler(){

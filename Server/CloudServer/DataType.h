5d0182b0109b35103084a6357ed4f77180672c6c
Triste <1182511338@qq.com>
2017-01-26 21:41:51 +0800

    modify datatype.h

--- Server/CloudServer/DataType.h
+++ Server/CloudServer/DataType.h
@@ -18,8 +18,6 @@ public:
 		x = (float)_x/32768*16;
 		y = (float)_y/32768*16;
 		z = (float)_z/32768*16;
-		float norm = sqrt(x*x+y*y+z*z);
-		cout<<"norm:"<<endl;
 		// x /= norm;
 		// y /= norm;
 		// z /= norm;
@@ -93,7 +91,7 @@ public:
 		rad_x = (float)_x*PI/32768;
 		rad_y = (float)_y*PI/32768;
 		rad_z = (float)_z*PI/32768;
-		cout<<"origin:"<<x<<" "<<y<<" "<<z<<", "<<"cal:"<<rad_x<<" "<<rad_y<<" "<<rad_z<<endl;	
+		//cout<<"origin:"<<x<<" "<<y<<" "<<z<<", "<<"cal:"<<rad_x<<" "<<rad_y<<" "<<rad_z<<endl;	
 		q[0] = cos(rad_x/2)*cos(rad_y/2)*cos(rad_z/2) + sin(rad_x/2)*sin(rad_y/2)*sin(rad_z/2);
 		q[1] = sin(rad_x/2)*cos(rad_y/2)*cos(rad_z/2) - cos(rad_x/2)*sin(rad_y/2)*sin(rad_z/2);
 		q[2] = cos(rad_x/2)*sin(rad_y/2)*cos(rad_z/2) + sin(rad_x/2)*cos(rad_y/2)*sin(rad_z/2);

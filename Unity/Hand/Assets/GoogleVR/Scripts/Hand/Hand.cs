28080e4d8f9834a9a7aac7644939177cb42ff116
Triste <1182511338@qq.com>
2017-03-06 10:57:14 +0800

    modify Hand.cs

--- Unity/Hand/Assets/GoogleVR/Scripts/Hand/Hand.cs
+++ Unity/Hand/Assets/GoogleVR/Scripts/Hand/Hand.cs
@@ -4,9 +4,7 @@ using System.Collections;
 
 public class Hand : MonoBehaviour {
     public enum ShiftingType{
-        X,
-        Y,
-        Z
+        X=0,Y,Z
     }
 
     float ex, ey, ez;
@@ -23,89 +21,114 @@ public class Hand : MonoBehaviour {
     int zSpeed = 10;
     int cnt = 0;
     int z_cnt = 1;
-    static private Vector3 referRotation = new Vector3(0, 0, 0);
+    static private Vector3 referRotation = new Vector3(0, 180, 0);
     bool isInit = false;
     float lastCameraY = 0.0f;
     float lastRcvY = 0.0f;
-    static float scale = 0.01f;
+    static float scale = 0.1f;
+    Vector3 initPosition = new Vector3(0.509f, -0.37f, 0.8f);
+    Vector3 negPisition = new Vector3(-0.5f,-0.37f,0.8f);
+    Vector3 evenPostion = new Vector3(0.509f,0.4f,1.2f);
+    float dist = 5.0f;
+    static int[] fingerStates = new int[5] { 0, 0, 0, 0, 0 };
+    static Vector3 handPos;
+
+    int xCnt = 0;
+    int zCnt = 0;
 
 	// Use this for initialization
 	void Start () {
-
-
-	}
-
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-
         SetXYZRotation();
         SetXYZPosition();
-        
-        //float x = (float)(hand.transform.forward.x * scale);
-        //Vector3 v = new Vector3((float)(hand.transform.forward.x * scale),
-        //    (float)(hand.transform.forward.y * scale), (float)(hand.transform.forward.z * scale));
-        //hand.transform.position += v;
+        handPos = hand.transform.position;
+        //float z = hand.transform.localEulerAngles.z;
+        //if(z>180)
+        //    z = 360-z;
+        //if ( z> 50)
+        //    --xCnt;
+        //else if (z < -50)
+        //    ++xCnt;
+        //else
+        //    xCnt = 0;
+
+        //float x = hand.transform.localEulerAngles.x;
+        //if (x > 180)
+        //    x = 360 - x;
+        //if (x > 50)
+        //    ++zCnt;
+        //else if (x < -50)
+        //    --zCnt;
+        //else
+        //    zCnt = 0;
+
+        //Debug.Log("xCnt:" + xCnt + " " + "hand.z:" + z);
+
+        //if (Mathf.Abs(xCnt) > 20)
+        //{
+        //    Vector3 v = hand.transform.right;//getShiftVec(dist * xCnt / (Mathf.Abs(xCnt)), ShiftingType.X);
+        //    v.x *= (scale * dist*xCnt / (Mathf.Abs(xCnt)));
+        //    hand.transform.localPosition += v;
+        //}
+
+        //if (Mathf.Abs(zCnt) > 20)
+        //{
+        //    Vector3 v = hand.transform.forward;// getShiftVec(dist * zCnt / (Mathf.Abs(zCnt)), ShiftingType.Z);
+        //    v.z *= (scale * dist * zCnt / (Mathf.Abs(zCnt)));
+        //    hand.transform.position+=v;
+        //}
+
 
 	}
 
-    void TestXYZRotation()
-    {
-        Vector3 r = Index.getRotation(); //new Vector3(90, cnt, 0);
-        
-        cnt++;
-        float angle = r.y;
-        r.y = 0;
-        //hand.transform.localEulerAngles = r;
-        //Debug.Log("rotate to:" + r.x + "," + r.y + "," + r.z);
-        ex = -r.x;
-        ey = r.y;
-        ez = -r.z;
-
-        ex = ex * PIover180 / 2.0f;
-        ey = ey * PIover180 / 2.0f;
-        ez = ez * PIover180 / 2.0f;
-        qx = Mathf.Sin(ex) * Mathf.Cos(ey) * Mathf.Cos(ez) - Mathf.Cos(ex) * Mathf.Sin(ey) * Mathf.Sin(ez);
-        qy = Mathf.Cos(ex) * Mathf.Sin(ey) * Mathf.Cos(ez) - Mathf.Sin(ex) * Mathf.Cos(ey) * Mathf.Sin(ez);
-        qz = Mathf.Cos(ex) * Mathf.Cos(ey) * Mathf.Sin(ez) + Mathf.Sin(ex) * Mathf.Sin(ey) * Mathf.Cos(ez);
-
-
-        qw = Mathf.Cos(ex) * Mathf.Cos(ey) * Mathf.Cos(ez) + Mathf.Sin(ex) * Mathf.Sin(ey) * Mathf.Sin(ez);
-
-        Quaternion q = new Quaternion(qx, qy, qz, qw);
-
-        //hand.transform.rotation = q;
-        float hand_y = hand.transform.localEulerAngles.y;
-        cube.transform.rotation = q;
-        Vector3 cv = cube.transform.localEulerAngles;
-        //Debug.Log("cube:" + cv.x + "," + cv.y + "," + cv.z + " ");
-        cv.y = hand_y;
-        if (angle != -361&&angle!=0)
-        {
-            float res = cv.y + (float)(-angle / 5);
-            if (res > -45 && res < 45)
-            {
-                cv.y = res;
-                Debug.Log(cnt + "-hand.y+" + (float)(angle / 5));
-            }
-            else
-            {
-
-            }
-            
-            
-        }
-            
-        hand.transform.localEulerAngles = cv;
-        //Vector3 v = hand.transform.localEulerAngles;
-        //v.z = -v.z;
-        //v.x = -v.x;
-        //v.y = -v.y;
-        //hand.transform.localEulerAngles = v;
-    }
-
     public void SetXYZPosition()
     {
+        if (Index.IsReady())
+        {
+            Vector3 s = Index.getShifting();
+            Vector3 tempPos = hand.transform.localPosition;
+            Vector3 add = new Vector3(0, 0, 0);
+
+            for (int i = 0; i < 3; ++i)
+            {
+                s[i] *= scale;
+                if (tempPos[i] + s[i] < negPisition[i]){
+                    if (s[i] > 0)
+                        add[i] = s[i];
+                    else
+                        add[i] = negPisition[i] - tempPos[i];
+                }
+                    
+                else if (tempPos[i] + s[i] > evenPostion[i]){
+                    if (s[i] < 0)
+                        add[i] = s[i];
+                    else
+                        add[i] = evenPostion[i] - tempPos[i];
+                }
+                else
+                    add[i] = s[i];
+            }
+
+            //hand.transform.localPosition += add;
+            //Debug.Log("add:x "+add.x);
+
+                //Vector3 tempPos = hand.transform.localPosition;
+                //Vector3 add = new Vector3(0, 0, 0);
+                //for (int i = 0; i < 1; ++i)
+                //{
+
+                //}
+                //Vector3 add = getShiftVec(s[(int)type], type);
+                //hand.transform.localPosition += add;
+                //hand.transform.localPosition += getShiftVec(s.x, ShiftingType.X);
+                Index.ResetShifting();
+            
+        }
+
         if (Input.GetKey(KeyCode.Q))//<- X
         {
             hand.transform.position += getShiftVec(-1, ShiftingType.X);
@@ -130,19 +153,37 @@ public class Hand : MonoBehaviour {
         {
             hand.transform.position += getShiftVec(1, ShiftingType.Z);
         }
-        
-            
-
-        if (Index.IsReady())
+        else
         {
-            Vector3 s = Index.getShifting();
-            //hand.transform.position += getShiftVec(s.x, ShiftingType.X);
-            Index.ResetShifting();
-            //hand.transform.localPosition;
-            //hand.transform.position += getShiftVec(s.y, ShiftingType.Y);
-            //hand.transform.position += getShiftVec(s.z, ShiftingType.Z);
+
         }
     }
+
+
+
+    Vector3 getShiftVec(float distance, ShiftingType type)
+    {
+        Vector3 v = new Vector3(0, 0, 0);
+        switch (type)
+        {
+            case ShiftingType.X:
+                v = hand.transform.right;
+                break;
+            case ShiftingType.Y:
+                v = hand.transform.up;
+                break;
+            case ShiftingType.Z:
+                v = hand.transform.forward;
+                break;
+        }
+
+        v.x *= (scale * distance);
+        v.y *= (scale * distance);
+        v.z *= (scale * distance);
+        Debug.Log("shiftVec:" + v.x);//+ " " + v.y + " " + v.z
+        return v;
+    }
+
 
     //use the gyro to show the rotation of xyz axis
     void SetXYZRotation()
@@ -246,58 +287,29 @@ public class Hand : MonoBehaviour {
         referRotation = v;
     }
 
-    Vector3 getShiftVec(float distance, ShiftingType type)
+    static public void SetFingerState(int i, int v)
     {
-        Vector3 v = new Vector3(0, 0, 0);
-        switch(type){
-            case ShiftingType.X:
-                v = hand.transform.right;
-                break;
-            case ShiftingType.Y:
-                v = hand.transform.up;
-                break;
-            case ShiftingType.Z:
-                v = hand.transform.forward;
-                break;
-        }
-       
-        v.x *= (scale * distance);
-        v.y *= (scale * distance);
-        v.z *= (scale * distance);
-        Debug.Log("shiftVec:" + v.x);//+ " " + v.y + " " + v.z
-        return v;
+        fingerStates[i] = v;
     }
 
-    void PlayAnimation()
+    static public bool IsCatch()
+    {
+        int sum = 0;
+        for(int i=0;i<5;++i)
+            sum+=fingerStates[i];
+        return sum == 5;
+    }
+
+    static public Vector3 GetHandPos()
+    {
+        return handPos;
+    }
+
+    static public void PlayStar()
     {
 
     }
 
-    void GetKeyEvent()
-    {
-        if (Input.GetKey(KeyCode.Alpha8))//up
-        {
 
-        }
-        else if (Input.GetKey(KeyCode.Alpha2))//down
-        {
 
-        }
-        else if (Input.GetKey(KeyCode.Alpha4))//left
-        {
-
-        }
-        else if (Input.GetKey(KeyCode.Alpha6))//right
-        {
-
-        }
-        else if (Input.GetKey(KeyCode.Alpha7))//forward
-        {
-
-        }
-        else if (Input.GetKey(KeyCode.Alpha9))//back
-        {
-
-        }
-    }
 }

1e071145b90f77cb72dc790c83d6b8b944c9c606
Triste <1182511338@qq.com>
2017-03-07 10:54:54 +0800

    the callback event of the detection

---
+++ Unity/Hand/Assets/GoogleVR/Scripts/Fountains/MyEvent.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyEvent : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        this.GetComponent<BeatDetection>().CallBackFunction = MyCallbackEventHandler;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MyCallbackEventHandler(BeatDetection.EventInfo eventInfo)
+    {
+        EventTypeCnt.AddEventType((int)eventInfo.messageInfo);
+        //SideFountain.AddEventType((int)eventInfo.messageInfo);
+
+        switch (eventInfo.messageInfo)
+        {
+            case BeatDetection.EventType.Energy:
+
+                Debug.Log("Energy");
+                break;
+            case BeatDetection.EventType.HitHat:
+                Debug.Log("Hithat");
+                break;
+            case BeatDetection.EventType.Kick:
+                Debug.Log("Kick");
+                break;
+            case BeatDetection.EventType.Snare:
+                Debug.Log("Snare");
+                break;
+        }
+    }
+
+}

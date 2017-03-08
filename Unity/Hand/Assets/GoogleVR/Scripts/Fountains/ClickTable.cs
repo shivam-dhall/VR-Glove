bee6b9d70bfd536afce870bd4ad61f7220cd61b7
Triste <1182511338@qq.com>
2017-03-08 10:49:01 +0800

    add a table scipt to play the key

---
+++ Unity/Hand/Assets/GoogleVR/Scripts/Fountains/ClickTable.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickTable : MonoBehaviour {
+    GameObject[] cubes = new GameObject[7];
+
+	// Use this for initialization
+	void Start () {
+        int i = 0;
+        foreach (Transform child in this.transform)
+        {
+            cubes[i++] = child.gameObject;
+        }
+	}
+
+
+    public void Activate()
+    {
+        
+        foreach (GameObject obj in cubes)
+        {
+            obj.GetComponent<Key>().Activate();
+        }
+
+    }
+
+    public void InActivate()
+    {
+
+        foreach (GameObject obj in cubes)
+        {
+            obj.GetComponent<MeshRenderer>().enabled = false;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Init()
+    {
+
+    }
+}

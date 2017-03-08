85d2ab50ec87bfce88c1ca37f8b838714bc7a964
Triste <1182511338@qq.com>
2017-03-08 10:49:41 +0800

    add the ending of the ecene

---
+++ Unity/Hand/Assets/GoogleVR/Scripts/Fountains/Ending.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending : MonoBehaviour {
+    bool isStart = false;
+    int selfState = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.L)&&isStart)
+        {
+            ++selfState;
+        }
+	}
+
+    public void Init()
+    {
+        isStart = true;
+    }
+}
